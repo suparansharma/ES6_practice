4498cec7ae0ef2098135fc336713bf22ec9adf9d
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-06-23 16:44:18 +0600

    const var data type useses

---
+++ letconst.js
@@ -0,0 +1,13 @@
+let patientName = "Rahim Caca";
+patientName = "Fatema Khala";
+console.log(patientName);
+
+let sum = 0;
+for(var i = 0; i <10; i++){
+
+    sum = sum + i;
+
+}
+
+console.log(i);
+
