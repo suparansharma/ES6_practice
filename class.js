c0ceb5a1b10fed0765b934b75e85dcac0ac17c6d
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-06-24 00:42:13 +0600

    all file

---
+++ class.js
@@ -0,0 +1,17 @@
+class Student{
+    constructor(sId,sName){
+
+        this.id = sId;
+        this.name=sName;
+        this.scholl = "kushtia";
+    }
+
+
+}
+
+const Student1 = new Student(12,"Rahim");
+const Student2 = new Student(200,"karim");
+
+console.log(Student1,Student2);
+
+
